c6f292e369faf87c41d0b2d81e9f449ec2c70506
Reshma.C.R <68727137+ReshmaCR02@users.noreply.github.com>
2020-11-24 12:33:53 +0530

    Add files via upload

---
+++ day2 task.py
@@ -0,0 +1,19 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=10
+>>> print(a/10)
+1.0
+>>> print(b*50)
+500
+>>> print(c+60)
+70
+>>> a="BROWN"
+>>> print(a.replace("O","G"))
+BRGWN
+>>> a=10
+>>> b=2.0
+>>> print(float(a))
+10.0
+>>> print(int(b))
+2
+>>> 

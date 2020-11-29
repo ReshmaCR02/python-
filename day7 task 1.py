b33572e003b009bf3ef5126cd36700656867dc91
Reshma.C.R <68727137+ReshmaCR02@users.noreply.github.com>
2020-11-29 21:34:30 +0530

    Add files via upload

---
+++ day7 task 1.py
@@ -0,0 +1,14 @@
+#1.create a module with list & import the module in another .py file & change the value in list
+from my_list import mylist
+mylist[1]=6
+print(mylist)
+#2,installed pandas package using pip install pandas in command prompt
+print("installed pandas package using pip")
+#3.
+import random
+print(random.randint(1,101))
+#4.
+import sys
+print(sys.path)
+#5.install a package & uninstall a package using pip
+print("installed and uninstalled a package using pip")
